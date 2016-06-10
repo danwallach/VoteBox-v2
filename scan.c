a458ab7d152966771b6f8eb985bd4fe3b28f1c42
luejerry <luejerry@gmail.com>
2016-06-10 22:38:15 +0000

    Read code directly from scanner driver, and block its input from stdin

--- scan.c
+++ scan.c
@@ -14,25 +14,106 @@
 #include <errno.h>
 #include <signal.h>
 #include <wiringPi.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <linux/input.h>
+
+#define MAXCODE 64
+
+char *device = "/dev/input/by-id/usb-WIT_Electron_Company_WIT_122-UFS_V2.03-event-kbd";
+
+char keymap(int code) {
+    switch (code) {
+        case KEY_1: return '1';
+        case KEY_2: return '2';
+        case KEY_3: return '3';
+        case KEY_4: return '4';
+        case KEY_5: return '5';
+        case KEY_6: return '6';
+        case KEY_7: return '7';
+        case KEY_8: return '8';
+        case KEY_9: return '9';
+        case KEY_0: return '0';
+        case KEY_Q: return 'Q';
+        case KEY_W: return 'W';
+        case KEY_E: return 'E';
+        case KEY_R: return 'R';
+        case KEY_T: return 'T';
+        case KEY_Y: return 'Y';
+        case KEY_U: return 'U';
+        case KEY_I: return 'I';
+        case KEY_O: return 'O';
+        case KEY_P: return 'P';
+        case KEY_A: return 'A';
+        case KEY_S: return 'S';
+        case KEY_D: return 'D';
+        case KEY_F: return 'F';
+        case KEY_G: return 'G';
+        case KEY_H: return 'H';
+        case KEY_J: return 'J';
+        case KEY_K: return 'K';
+        case KEY_L: return 'L';
+        case KEY_Z: return 'Z';
+        case KEY_X: return 'X';
+        case KEY_C: return 'C';
+        case KEY_V: return 'V';
+        case KEY_B: return 'B';
+        case KEY_N: return 'N';
+        case KEY_M: return 'M';
+        default: return '_';
+    }
+}
 
 int main(void) {
-    struct pollfd mypoll = { STDIN_FILENO, POLLIN|POLLPRI };
-    char code[32];
-    int err = 0;
-    
-    wiringPiSetupGpio(); // BCM pin numbering
+    int scanfd = open(device, O_RDONLY); 
+    FILE *scanst = fdopen(scanfd, "r");
+    struct pollfd mypoll = { scanfd, POLLIN|POLLPRI };
+    char code[MAXCODE];
+    int err = 0, readstatus = 0, codecounter = 0;
+    struct input_event keyevent;
 
+    ioctl(scanfd, EVIOCGRAB, (void *) 1); // get exclusive access to scanner
+    wiringPiSetupGpio(); // BCM pin numbering
     pinMode(25, OUTPUT);
+
     // Loop until code read or error occurs
     while (!err) {
         // Turn on scanner
         digitalWrite(25, HIGH);
         // Wait for scan for 2.5s before restarting scanner
-        if(err = poll(&mypoll, 1, 2500)) {
-            scanf("%31s", code);
-            printf("%s", code);
+        if(err = poll(&mypoll, 1, 2000)) {
+            while ((readstatus = read(scanfd, &keyevent, sizeof(keyevent))) >= 0) {
+                #ifdef DEBUG
+                printf("Read returned: %d; ", readstatus);
+                printf("Event type: %u; ", keyevent.type);
+                printf("Event code: %u; ", keyevent.code);
+                printf("Event value: %u\n", keyevent.value);
+                #endif
+                if (keyevent.type == EV_KEY && keyevent.value == 1) { // key press
+                    if (codecounter == MAXCODE - 1 || keyevent.code == KEY_ENTER) {
+                        // enter key signals end of scan 
+                        code[codecounter] = '\0';
+                        break;
+                    }
+                    // convert keycode to ascii char
+                    code[codecounter] = keymap(keyevent.code);
+                    if (code[codecounter] != '_') { // strip unrecognized characters
+                        codecounter++;
+                    }
+                }
+            }
+            if (readstatus < sizeof(keyevent)) {
+                fprintf(stderr, "Error occurred scanning: %s", strerror(errno));
+                digitalWrite(25, LOW);
+                return -1;
+            }
+            printf("%s\n", code);
         } else if (err < 0) {
-            fprintf(stderr, "Error occurred reading: %s", strerror(errno));
+            fprintf(stderr, "Error occurred polling: %s", strerror(errno));
+            digitalWrite(25, LOW);
+            return -1;
         } 
         //else {
         //    printf("No code scanned\n");
