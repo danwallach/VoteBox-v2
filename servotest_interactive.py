042d2221fbacdb799db927fabaa4d3331f0bdf2c
luejerry <luejerry@gmail.com>
2016-06-21 15:21:31 -0500

    add interactive servo testing program

---
+++ servotest_interactive.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import rpi_servodriver as sdriver
+import sys
+
+SERVO_PIN = 18
+
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    servo = sdriver.init(SERVO_PIN)
+    sdriver.start(servo, 0.5)
+    while True:
+        try:
+            angle = input("Enter servo angle (0.0-1.0): ")
+            sdriver.move(servo, angle)
+        except KeyboardInterrupt:
+            sdriver.stop(servo)
+            GPIO.cleanup()
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
