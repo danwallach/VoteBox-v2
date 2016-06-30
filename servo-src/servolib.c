1c6957f0df60359f635b77d7aba445cf3349fadb
luejerry <luejerry@gmail.com>
2016-06-30 14:46:01 -0500

    add makefile/header for c servolib

--- servo-src/servolib.c
+++ servo-src/servolib.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * servolib
+ *
+ * Reimplementation of the servo library in C using wiringPi. This
+ * approach uses the hardware PWM feature available on GPIO 18 of the
+ * Raspberry Pi, reducing GPU usage and increasing timing accuracy over
+ * the software based solution.
+ *
+ * Author:
+ * Jerry Lue
+ */
+#include "servolib.h"
 
-#include <wiringPi.h>
-
-#define PWM_PIN 18
+// #define PWM_PIN 18
 
 int servo_init() {
 	pinMode(PWM_PIN, PWM_OUTPUT);
 	pwmSetMode(PWM_MODE_MS);
-	pwmSetClock(1920);
-	pwmSetRange(200);
+	pwmSetClock(375);
+	pwmSetRange(1024);
 	return 0;
 }
 
@@ -17,12 +25,26 @@ void servo_move(int pos) {
 	pwmWrite(PWM_PIN, pos);
 }
 
+/*
+ * Move a HD 6001HB servo.
+ *
+ * For the 6001HB servo, raw input ranges from 40 (0.8 ms) to 120
+ * (2.35 ms), which translates to a range of 0-80 for the input here.
+ * Values outside this range may be used but response is not guaranteed.
+ */
+void hd6001_move(int pos) {
+    servo_move(pos + 40);
+}
+
 void servo_interactive() {
-	char input[6];
-	int value;
-	fgets(input, 6, stdin);
-	value = atoi(input);
-	servo_move(value);
+    while (1) {
+        char input[6];
+        int value;
+        printf("Enter servo value (0-1024): ");
+        fgets(input, 6, stdin);
+        value = atoi(input);
+        servo_move(value);
+    }
 }
 
 int main() {
---
+++ servo-src/servolib.h
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+#define PWM_PIN 18
+
+int servo_init();
+void servo_move(int pos);
+void hd6001_move(int pos);
+void servo_interactive();
