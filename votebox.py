1b3d7c228b2b03eb8f086fcd4f715148c739c1a2
Jerry Lue <luejerry@gmail.com>
2016-07-19 16:22:48 -0500

    Attempt fix socket not closing issue

--- votebox.py
+++ votebox.py
@@ -9,11 +9,12 @@ import take_in
 
 
 def main():
-    broadcast_status = threading.Thread(target=status_server.main)
+    broadcast_status = threading.Thread(target=status_server.serve)
     broadcast_status.daemon = True
     broadcast_status.start()
 
     take_in.main()
+    config.httpd.socket.close()
 
 if __name__ == '__main__':
     main()
